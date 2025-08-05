e9b92a6cb6e3d75df2a5e91c92cc27c5ebf13ef3
vamshithallapalli <thallapallivamshi50@gmail.com>
2025-08-05 16:58:11 +0530

    modified

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Flames from "./Flames";
 
 class App extends Component {
     render() {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <Flames />
             </div>
         )
     }
---
+++ src/components/Flames.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+const Flames = () => {
+  const [fname, setFname] = useState("");
+  const [sname, setSname] = useState("");
+  const [result, setResult] = useState("");
+  const data = [
+    "Siblings",
+    "Friends",
+    "Love",
+    "Affection",
+    "Marriage",
+    "Enemy",
+  ];
+
+  const handleResult = (e) => {
+    e.preventDefault();
+
+    if (fname.trim() === "" || sname.trim() === "") {
+      setResult("Please Enter valid input");
+      return;
+    }
+
+    const str1 = fname.toLowerCase().trim();
+    const str2 = sname.toLowerCase().trim();
+
+    const freqA = {};
+    const freqB = {};
+
+    for (let ch of str1) {
+      freqA[ch] = (freqA[ch] || 0) + 1;
+    }
+
+    for (let ch of str2) {
+      freqB[ch] = (freqB[ch] || 0) + 1;
+    }
+
+    //Remove common characters
+    for (let ch of str1) {
+      if (freqB[ch]) {
+        const minLen = Math.min(freqA[ch], freqB[ch]);
+        freqA[ch] -= minLen;
+        freqB[ch] -= minLen;
+      }
+    }
+
+    //count remaining characters
+    let count = 0;
+    for (let ch in freqA) {
+      count += freqA[ch];
+    }
+    for (let ch in freqB) {
+      count += freqB[ch];
+    }
+
+    const Index = count % 6;
+    console.log(count);
+
+    setResult(data[Index]);
+  };
+
+  const handleClear = () => {
+    setFname("");
+    setSname("");
+    setResult("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="first Name"
+        value={fname}
+        onChange={(e) => setFname(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="second Name"
+        value={sname}
+        onChange={(e) => setSname(e.target.value)}
+      />
+      <button onClick={handleResult}>Calculate Future Relationship</button>
+      <button onClick={handleClear}>Clear</button>
+      <h1>{result}</h1>
+    </div>
+  );
+};
+
+export default Flames;
